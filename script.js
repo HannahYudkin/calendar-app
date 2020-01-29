b241934840889cd5aaf27e4d554dd3fc97ab6059
Hannah Yudkin <hannahyudkin@Hannahs-MacBook-Pro-2.local>
2020-01-29 14:27:22 -0500

    Added local storage functionality.

--- script.js
+++ script.js
@@ -1,65 +1,46 @@
 var now = moment().format("dddd, MMMM DD, YYYY");
 
-
-
-
-
-// let date = moment().date();
-// let day = moment().day();
-// let month = moment().month();
-// let year = moment().year();
-
-// console.log(day);
-// console.log(date);
-// console.log(now);
-// console.log(hour);
-
-
-
-
-
-
-
-
 $("#currentDay").html(`
     <h3> ${now} </h3>
 `);
 
-for (let i=9; i <= 17; i++) {
-    let iTime = moment(i, 'HH:mm').format("h:mm a")
-    
-    let classColor;
-    let plannerHour = moment().hour();
-    
-    
-    if (i < moment().hour()) {
-        classColor = "past";
-    }
-    else if (i === moment().hour()) {
-        classColor = "present";
-    }
-    else {
-        classColor = "future";
-    }
-    
+for (let i = 9; i <= 17; i++) {
+  let iTime = moment(i, "HH:mm").format("h:mm a");
+
+  let classColor;
+  let value;
 
 
+  if (localStorage.getItem(moment(i, "HH:mm").format("h:mm"))) {
+    value = localStorage.getItem(moment(i, "HH:mm").format("h:mm"));
+  } else {
+    value = "";
+  }
 
+  if (i < moment().hour()) {
+    classColor = "past";
+  } else if (i === moment().hour()) {
+    classColor = "present";
+  } else {
+    classColor = "future";
+  }
 
-
-
-
-
-
-
-    $("#planner-container").append( `
+  $("#planner-container").append(`
     <div class="row">
         <div class="hour 9 col">${iTime}</div>
-        <textarea class="textarea col ${classColor}"></textarea>
+        <textarea id = ${iTime} class="textarea col ${classColor}">${value}</textarea>
         <button class="saveBtn col">Save</button>
      </div>
-    `)
+    `);
 }
 
-
-
+$(document).on("click", ".saveBtn", function() {
+  let id = $(this)
+    .prev()
+    .attr("id");
+  let text = $(this)
+    .prev()
+    .val();
+  localStorage.setItem(id, text);
+  console.log(id, text);
+});
